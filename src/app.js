93318a838e3d311ffd40d820ca8f05798654c11d
wahe7 <waheguru002200@gmail.com>
2025-07-12 01:25:15 +0530

    setup app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/user", require("./routes/user.routes"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("GEMINI Backend");
 });
 
 module.exports = app;
