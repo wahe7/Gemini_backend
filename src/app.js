67dde7d0977ace5e7ff46dc79409cdcb82c3594a
wahe7 <waheguru002200@gmail.com>
2025-07-13 00:14:10 +0530

    fix indentation

--- src/app.js
+++ src/app.js
@@ -7,9 +7,8 @@ app.post(
   express.raw({ type: "application/json" }), 
   require("./controllers/stripe.webhook").webhook_response
 );
-
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/user", require("./routes/user.routes"));
