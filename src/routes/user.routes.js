5897bbe6d3c401cc9400ca7bf94986a42d73e4e4
wahe7 <waheguru002200@gmail.com>
2025-07-12 19:55:24 +0530

    add route for subscription status

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getUser } = require("../controllers/user.controller");
+const { getUser, getUserSubscription } = require("../controllers/user.controller");
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 router.get("/me",verifyToken, getUser);
+router.get("/subscription",verifyToken, getUserSubscription);
 
 module.exports = router;
